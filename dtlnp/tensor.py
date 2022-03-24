0c58bd95c38149693fde894e231d769b6360dfc8
Connor Ward <c.ward20@imperial.ac.uk>
2022-03-24 13:53:22 +0000

    Get deindexing working properly

--- dtlnp/tensor.py
+++ dtlnp/tensor.py
@@ -6,6 +6,7 @@ import numpy as np
 
 
 BINOPS = {
+    dtl.AddBinOp: np.add,
     dtl.MulBinOp: np.multiply
 }
 
@@ -27,7 +28,7 @@ class NameGenerator:
         return f"{self._prefix}{next(self._counter)}{self._suffix}"
 
 
-# TODO don't actually pass this in to expr, a mapping at the end instead
+# TODO don't actually pass this in to expr, instead pass a mapping at compute-time
 class Tensor(dtl.TensorVariable):
 
     _name_generator = NameGenerator(prefix="T")
@@ -45,48 +46,6 @@ class Tensor(dtl.TensorVariable):
         return type(self)(name, space, data)
 
 
-class TensorSum(dtl.TensorExpression):
-
-    def __init__(self, lhs, rhs, lhs_indices, rhs_indices, out_indices):
-        self.lhs = lhs
-        self.rhs = rhs
-        self.lhs_indices = lhs_indices
-        self.rhs_indices = rhs_indices
-        self.out_indices = out_indices
-
-
-@functools.singledispatch
-def replace_deindexes(expr):
-    """Replace deindex -> add with TensorSum nodes. This lets us do our
-    evaluations at the right level with singledispatch."""
-    raise AssertionError
-
-
-@replace_deindexes.register
-def _(expr: dtl.deIndex):
-    if not isinstance(expr.scalar_expr, dtl.BinOp):
-        return expr  # need to postorder here too
-
-    lhs = expr.scalar_expr.lhs
-    rhs = expr.scalar_expr.rhs
-
-    assert isinstance(lhs, dtl.IndexedTensor)
-    assert isinstance(rhs, dtl.IndexedTensor)
-    
-    # postorder traversal
-    lhs_tensor_expr = replace_deindexes(lhs.tensor_expr)
-    rhs_tensor_expr = replace_deindexes(rhs.tensor_expr)
-
-    if isinstance(expr.scalar_expr, dtl.AddBinOp):
-        return TensorSum(lhs_tensor_expr, rhs_tensor_expr, lhs.indices, rhs.indices, expr.indices)
-    else:
-        raise NotImplementedError
-
-
-def evaluate(expr):
-    expr = replace_deindexes(expr)
-    return apply(expr)
-
 @functools.singledispatch
 def apply(expr):
     """Apply a postorder traversal to the DAG, computing a result as you go."""
@@ -100,45 +59,81 @@ def _(expr: dtl.Lambda):
 
 @apply.register
 def _(unindex: dtl.deIndex):
-    # scalar_expr = apply(unindex.scalar_expr)
-    #
-    # input_indices = _stringify(scalar_expr.indices)
-    # output_indices = _stringify(unindex.indices)
-    # return scalar_expr.tensor.copy(data=np.einsum(f"{input_indices}->{output_indices}", scalar_expr.tensor.data))
+    scalar_expr = apply(unindex.scalar_expr)
 
-    if isinstance(unindex.scalar_expr, dtl.BinOp):
-        lhs, rhs = 
+    result_shape = tuple(i.space.dim for i in unindex.indices)
+    result = Tensor(None, data=np.zeros(result_shape, dtype=int))
+    indexed_result = result[unindex.indices]
 
-    lhs, rhs = apply(unindex.scalar_expr)
-
-    for i, j in unindex.indices:
-        output[i, j] = apply(unindex.scalar_expr, [i, j])
-        output[i, j] = lhs[i] * rhs[j]
+    # For the expression (A[i, j] + B[j, k] + C[l, m]).forall(i, k, m) this magic line produces:
+    """
+    for i:
+        for j:
+            for k:
+                for l:
+                    for m:
+                        res[i, k, m] += A[i, j] + B[j, k] + C[l, m]
+                    end for
+                end for
+            end for
+        end for
+    end for
+    """
+    for item in itertools.product(*(iterate_index(i) for i in set(scalar_expr.indices))):
+        # We have the index map to look up the values of i, j, k etc so we can index
+        # into the numpy arrays.
+        index_map = dict(item)
+        value = compute_scalar_expr(scalar_expr, index_map)
+        # warning: modifies in place!
+        inc_scalar(indexed_result, value, index_map)
+    return result
 
 
 @apply.register
 def _(expr: dtl.BinOp):
-    # I think that binops only make sense if the lhs and rhs have the same indices
-    # That doesn't work because matmul breaks...
-    # Should only apply to matching indices, but then what is the output shape?
-    assert expr.lhs.indices == expr.rhs.indices
-
-    op = BINOPS[type(expr)]
-    lhs, rhs = apply(expr.lhs), apply(expr.rhs)
-    tensor = Tensor(expr.lhs.tensor.space, data=op(lhs.tensor.data, rhs.tensor.data))
-    return dtl.IndexedTensor(tensor, lhs.indices)
+    return type(expr)(apply(expr.lhs), apply(expr.rhs))
 
 
 @apply.register
 def apply_indexed_tensor(expr: dtl.IndexedTensor):
     # remember this is a scalar
-    return type(expr)(apply(expr.tensor), expr.indices)
+    return type(expr)(apply(expr.tensor_expr), expr.indices)
 
 
 @apply.register
 def apply_tensor_variable(expr: dtl.TensorVariable):
     # do nothing for actual tensor variables. These sit as the base of the tree
     return expr
+
+
+def compute_scalar_expr(scalar_expr, index_map):
+    """Return a scalar from a scalar expression and a given map between
+    abstract indices and their values.
+
+    This function is recursive in order to handle a sequence of binary operations.
+    """
+    if isinstance(scalar_expr, dtl.IndexedTensor):
+        # base case
+        return get_scalar(scalar_expr, index_map)
+    else:
+        assert isinstance(scalar_expr, dtl.BinOp)
+        lhs = compute_scalar_expr(scalar_expr.lhs, index_map)
+        rhs = compute_scalar_expr(scalar_expr.rhs, index_map)
+        return BINOPS[type(scalar_expr)](lhs, rhs)
+
+
+def get_scalar(expr: dtl.ScalarExpr, index_map):
+    """Retrieve a scalar from an expression given an index map."""
+    return expr.tensor_expr.data[tuple(index_map[i] for i in expr.indices)]
+
+
+def inc_scalar(expr, value, index_map):
+    """Increment an entry in a scalar expression."""
+    expr.tensor_expr.data[tuple(index_map[i] for i in expr.indices)] += value
+
+
+def iterate_index(index):
+    return ((index, d) for d in range(index.space.dim))
 
 
 def _stringify(indices):
