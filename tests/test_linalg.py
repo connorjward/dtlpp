e3ac25eaf2dd2ccf256bd3785bc4991f990ca7d3
Connor Ward <c.ward20@imperial.ac.uk>
2022-03-24 12:53:32 +0000

    Add some more test cases

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -6,15 +6,25 @@ from dtlnp.tensor import Tensor, apply
 
 
 @pytest.fixture
-def space():
-    return dtl.RealVectorSpace(10)
+def x():
+    return Tensor([dtl.RealVectorSpace(10)], "x", np.arange(10, dtype=int))
 
 
-def test_matmul():
+@pytest.fixture
+def y():
+    return Tensor([dtl.RealVectorSpace(10)], "y", np.arange(10, dtype=int))
+
+
+@pytest.fixture
+def z():
+    return Tensor([dtl.RealVectorSpace(10)], "z", np.arange(10, dtype=int))
+
+
+def test_matvec():
     A = Tensor("A", [dtl.RealVectorSpace(10)]*2, np.arange(100, dtype=int).reshape((10, 10)))
     x = Tensor("x", [dtl.RealVectorSpace(10)], np.arange(10, dtype=int))
-    i = dtl.Index("i")
-    j = dtl.Index("j")
+    i = dtl.Index("i", dtl.RealVectorSpace(10))
+    j = dtl.Index("j", dtl.RealVectorSpace(10))
     expr = dtl.Lambda([A, x], (A[i, j]*x[j]).forall(i))
 
     res = apply(expr)
@@ -24,7 +34,7 @@ def test_matmul():
 def test_trace():
     data = np.arange(100, dtype=int).reshape((10, 10))
     A = Tensor([dtl.RealVectorSpace(10)]*2, "A", data.copy())
-    i = dtl.Index("i")
+    i = dtl.Index("i", dtl.RealVectorSpace(10))
     expr = dtl.Lambda([A], A[i, i].forall())
 
     tensor = apply(expr)
@@ -34,8 +44,8 @@ def test_trace():
 def test_transpose():
     data = np.arange(100, dtype=int).reshape((10, 10))
     A = Tensor([dtl.RealVectorSpace(10)]*2, "A", data.copy())
-    i = dtl.Index("i")
-    j = dtl.Index("j")
+    i = dtl.Index("i", dtl.RealVectorSpace(10))
+    j = dtl.Index("j", dtl.RealVectorSpace(10))
     expr = dtl.Lambda([A], A[i, j].forall(j, i))
 
     tensor = apply(expr)
@@ -46,8 +56,26 @@ def test_vecdot():
     x = Tensor([dtl.RealVectorSpace(10)], "x", np.arange(10, dtype=int))
     y = Tensor([dtl.RealVectorSpace(10)], "y", np.arange(10, dtype=int))
 
-    i = dtl.Index("i")
+    i = dtl.Index("i", dtl.RealVectorSpace(10))
 
     expr = dtl.Lambda([x, y], (x[i]*y[i]).forall())
 
     assert apply(expr).data == np.dot(np.arange(10, dtype=int), np.arange(10, dtype=int))
+
+
+def test_pointwise_vec_add(x, y):
+    i = dtl.Index("i", dtl.RealVectorSpace(10))
+    expr = dtl.Lambda([x, y], (x[i]+y[i]).forall(i))
+    assert all(apply(expr).data == x.data + y.data)
+
+
+def test_pointwise_vec_mul(x, y):
+    i = dtl.Index("i", dtl.RealVectorSpace(10))
+    expr = dtl.Lambda([x, y], (x[i]*y[i]).forall(i))
+    assert all(apply(expr).data == x.data * y.data)
+
+
+def test_ternary_add(x, y, z):
+    i = dtl.Index("i", dtl.RealVectorSpace(10))
+    expr = dtl.Lambda([x, y, z], (x[i]+y[i]+z[i]).forall(i))
+    assert all(apply(expr).data == x.data + y.data + z.data)
